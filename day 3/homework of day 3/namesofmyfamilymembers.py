930b3d0f59995246d9a1ae54fe257894f15cf307
imtherealiko <iakobidoghonadze@gmail.com>
2024-02-21 22:44:59 +0400

    commit

---
+++ day 3/homework of day 3/namesofmyfamilymembers.py
@@ -0,0 +1,8 @@
+mother = ' nino'
+father = ' david'
+brother = ' daniel '
+sister = ' ruti '
+
+
+
+print("my mothers name is" + mother + ", my fathers name is" + father + ", my brothers name is " + brother + "and my sisters name is" + sister)

67ff46e0ffc17fcbca4bc68d9cfc522e2443e0e1
imtherealiko <iakobidoghonadze@gmail.com>
2024-03-03 19:43:26 +0400

    commit

---
+++ day 5/homework/engwords.py
@@ -0,0 +1,31 @@
+#ეს სიტყვები სასწავლად + ამ სიტყვების გამოყენებით ააწყვეთ წინადადებები
+
+
+#weather - ამინდი
+#city - ქალაქი
+#Rearrange  - გადაადგილე
+#parentheses - ფრჩხილები
+#variety - სხვადასხვა
+#incorrect  - არასწორი
+#loss - წაგება,დაკარგა
+#select - არჩევა
+#handle - გამკლავება
+#tell - თქმა
+#survey - გამოკითხვა ensure - უზუნველყოფა
+#similar - მსგავსი
+#explicit - გამოკვეთილი
+
+
+print("my friend has a similar jacket.")
+print("i live in a city called marneuli")
+print("when buying a car there are variety of colors to choose from.")
+print("a human skull can handle up to 520 pounds of force.")
+print("if you are living on an incorrect path of life join GOA to change your life and become a chad.")
+print("rearranging is always helpful.")
+print("when using command print you need to use parentheses.")
+print("try to make as least people possible to hate you.")
+print("scam vebsites use surveys to get as much money out of you as they can and then they steal your personal information.")
+print("i like chilly weather and darker shade of sky how about you?")
+print("never be explicit from other people by bieng a bad person.")
+print("ensure i dont know about ensure :<")
+print("tell your friends to join goa and become a better person in life :>.")
